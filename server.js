58b4dd289e19cedfc6b9a6187612c4407f2425c7
ErmiyasHailemichael <Hailermiyas@gmail.com>
2024-09-24 15:00:31 -0700

    not found page added

--- server.js
+++ server.js
@@ -12,6 +12,11 @@ app.get('/', (req, res) =>{
     res.sendFile(path.join(__dirname + 'public', 'index.html'))
 })
 
+// 404 
+app.use((req, res) =>{
+    res.status(404).sendFile(path.join(__dirname, 'public', '404.html'))
+})
+
 app.listen(PORT, () =>{
     console.log(`Server is running at ${PORT}`);
 });

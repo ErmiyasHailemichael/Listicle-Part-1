2d6cb5878be94aa30e50dbc7738b4fde865b67b4
ErmiyasHailemichael <Hailermiyas@gmail.com>
2024-09-24 12:10:29 -0700

    image and p are loading

---
+++ public/app.js
@@ -0,0 +1,33 @@
+const guideList = [
+    {
+        title: 'Brainstorm Business Ideas',
+        text: 'Finding your niche is key. Here is how to do it...',
+        category: 'Business Models',
+        image: 'https://images.pexels.com/photos/590041/pexels-photo-590041.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+        submittedBy: 'Ermi'
+    },
+    {
+        title: 'Marketing',
+        text: 'How marketing can help you boosting your business',
+        category: 'Marketing ',
+        image: 'https://images.pexels.com/photos/590041/pexels-photo-590041.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+        submittedBy: 'Ermiyas'
+    }
+];
+
+const guideListElement = document.getElementById('listsOfGuide')
+guideList.forEach((tip) =>{
+    const card = document.createElement('div');
+    card.classList.add('card');
+    card.innerHTML = `
+    <img src="${tip.image}" alt="${tip.title}">
+    <h3>${tip.title}</h3>
+    <p>${tip.text}</p>
+    <button onclick="viewDetail('${tip.title}')">View Details</button>
+    `;
+    guideListElement.appendChild(card)
+});
+
+function viewDetail(title){
+    alert(`Details about: ${title}`)
+}
--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 // import express from 'express'
 
 const express = require('express')
+const path = require('path')
 
 const app = express()
-
 const PORT = 3000;
 
-app.use(express.static('public'))
+app.use(express.static(path.join(__dirname,'public')))
 
 app.get('/', (req, res) =>{
-    res.sendFile(__dirname + '/public/index.html')
+    res.sendFile(path.join(__dirname + 'public', 'index.html'))
 })
 
 app.listen(PORT, () =>{
